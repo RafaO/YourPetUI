e8830e275bc4d4da6b7a34a5808b67e332668883
Rafael Ortega <rafao1989@hotmail.com>
2021-02-27 17:38:15 +0100

    set custom style for h1 text

--- app/src/main/java/com/keller/yourpetui/ui/Type.kt
+++ app/src/main/java/com/keller/yourpetui/ui/Type.kt
@@ -12,6 +12,9 @@ val typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
+    ),
+    h1 = TextStyle(
+        fontSize = 38.sp
     )
     /* Other default text styles to override
     button = TextStyle(
