e4275ee9c3bea4809819610d553dd1ebcddfa88c
Rafael Ortega <rafao1989@hotmail.com>
2021-03-02 20:18:54 +0100

    use string resource

--- app/src/main/java/com/keller/yourpetui/petlist/PetsListScreen.kt
+++ app/src/main/java/com/keller/yourpetui/petlist/PetsListScreen.kt
@@ -1,17 +1,23 @@
 package com.keller.yourpetui.petlist
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.keller.yourpetui.R
 import com.keller.yourpetui.model.Pet
 import com.keller.yourpetui.ui.composable.PetCard
 
 @Composable
 fun PetsListScreen(pets: List<Pet>, onPetClicked: (Pet) -> Unit) = Column {
     Text(
-        "Pets in adoption",
+        stringResource(id = R.string.title_pets_list),
+        modifier = Modifier.padding(16.dp),
         style = MaterialTheme.typography.h1
     )
     PetsList(pets, onPetClicked)
